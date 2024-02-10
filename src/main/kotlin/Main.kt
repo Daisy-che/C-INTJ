55211f41732be110eec3f13ed9f4be0020b07498
Daisy-che <daisychepkiruih@gmail.com>
2024-02-10 11:18:37 +0300

    added files

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,22 @@
+fun main(args: Array<String>) {
+    var name = "Daisy"
+    println(name)
+    var age = 22
+    println(age)
+    var phoneNumber = +254748064111
+    println(phoneNumber)
+    var weight = 50
+    println(weight)
+    var nationality = "Kenyan"
+      println(nationality)
+    val citizen = true
+println(citizen)
+
+
+
+
+
+
+
+
+}
